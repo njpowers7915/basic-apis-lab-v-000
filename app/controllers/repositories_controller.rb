8488172517e2eec6979131b48609a12c9607a55e
Nicholas Powers <njpowers7915@gmail.com>
2018-08-12 20:48:43 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,21 @@
 class RepositoriesController < ApplicationController
 
   def search
+    @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+        req.params['client_id'] = client_id
+        req.params['client_secret'] = client_secret
+        #req.params['v'] = '20160201'
+        req.params['q'] = params[:query]
+        #req.options.timeout = 0
+      end
 
+      body = JSON.parse(@resp.body)
+      if @resp.success?
+        @items = body["items"]
+      #else
+      #  @error = body["meta"]["errorDetail"]
+      end
+    render 'search'
   end
 
   def github_search
